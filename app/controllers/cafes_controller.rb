3399ab4febe2940c985614433d8b9b3ee027fe79
github username <github email address>
2019-08-23 17:38:34 +0900

    adds RESTful routes to cafes controller

--- app/controllers/cafes_controller.rb
+++ app/controllers/cafes_controller.rb
@@ -1,4 +1,46 @@
 class CafesController < Sinatra::Base
 
+    get '/cafes' do
+        @cafes = Cafe.all
+        erb :index
+    end
 
+
+    get '/cafes/new' do
+        erb :'cafes/create_cafe'
+    end
+
+    post '/cafes' do
+        @cafe = Cafe.create(params)
+        redirect to "/cafes/#{@cafe.id}"
+    end
+
+    get '/cafes/:id' do
+        @cafe = Cafe.find_by_id(params[:id])
+        erb :'cafes/show_cafe'
+    end
+
+    get '/cafes/:id/edit' do
+        @cafe = Cafe.find_by_id(params[:id])
+        erb :'cafes/edit_cafe'
+    end
+
+    patch '/cafes/:id' do
+        @cafe = Cafe.find_by_id(params[:id])
+        @cafe.name = params[:name]
+        @cafe.location = params[:location]
+        @cafe.parking = params[:parking]
+        @cafe.americano_price = params[:americano_price]
+        @cafe.dog_friendly = params[:dog_friendly]
+        @cafe.outlets = params[:outlets]
+        @cafe.rewiew = params[:review]
+        @cafe.save
+        redirect to "cafes/#{@cafe.id}"
+    end
+
+    delete '/cafes/:id/delete' do
+        @cafe = Cafe.find_by_id(params[:id])
+        @cafe.delete
+        redirect to '/cafes'
+      end
 end
---
+++ db/migrate/20190823082023_create_cafes.rb
@@ -0,0 +1,13 @@
+class CreateCafes < ActiveRecord::Migration[5.2]
+  def change
+    create_table :cafes do |t|
+      t.string :name
+      t.string :location
+      t.string :parking
+      t.string :americano_price
+      t.string :dog_friendly
+      t.string :outlets
+      t.text :review
+    end
+  end
+end
